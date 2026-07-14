2105daa896c56d5a7f644c3aeceeb107ba6a6b60
Stanislav Stefanov <stanislav.stefanov@gmail.com>
2026-07-14 21:55:39 +0300

    test: verify householdRef forwarding in UpdateContactTests

--- tests/Harmonia.UnitTests/Application/UpdateContactTests.cs
+++ tests/Harmonia.UnitTests/Application/UpdateContactTests.cs
@@ -1,6 +1,7 @@
 using Harmonia.Application;
 using Harmonia.Application.Directory;
 using Harmonia.Domain;
+using Harmonia.Domain.Directory;
 
 namespace Harmonia.UnitTests.Application;
 
@@ -41,5 +42,16 @@ public class UpdateContactTests
         var useCase = new UpdateContact(new FakeSession(AdminCtx), new FailingDirectoryStore());
         Assert.IsType<UpdateContactResult.Failed>(
             await useCase.ExecuteAsync("HH-TARGET-1", "Bob", null, null));
+    }
+
+    [Fact]
+    public async Task HouseholdRef_from_parameter_is_forwarded_to_store()
+    {
+        var store = new FakeDirectoryStore();
+        var useCase = new UpdateContact(new FakeSession(AdminCtx), store);
+        await useCase.ExecuteAsync("HH-FORWARDED-1", "Bob", null, null);
+
+        Assert.Single(store.Contacts);
+        Assert.Equal(new HouseholdRef("HH-FORWARDED-1"), store.Contacts[0].HouseholdRef);
     }
 }
