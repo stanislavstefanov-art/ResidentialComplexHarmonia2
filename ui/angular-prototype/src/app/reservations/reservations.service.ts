e739a36848ce6b05d5ec0998f0d586b2bbd7903d
Stanislav Stefanov <stanislav.stefanov@gmail.com>
2026-07-16 20:59:21 +0300

    feat(angular): wire vitest, add ReservationsService with passing tests

---
+++ ui/angular-prototype/src/app/reservations/reservations.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ClaimResponse, DaySlotsResponse } from './models';
+
+const API = 'http://localhost:5000';
+
+@Injectable({ providedIn: 'root' })
+export class ReservationsService {
+  private readonly http = inject(HttpClient);
+
+  getSlots(day: string): Observable<DaySlotsResponse> {
+    return this.http.get<DaySlotsResponse>(`${API}/days/${day}/slots`);
+  }
+
+  claimSlot(day: string, slotKey: string): Observable<ClaimResponse> {
+    return this.http.post<ClaimResponse>(`${API}/days/${day}/slots/${slotKey}/claim`, null);
+  }
+}
