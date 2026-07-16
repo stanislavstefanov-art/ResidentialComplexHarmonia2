7d5df92450d8e49842f81ce5121b4f6bba4a2fa9
Stanislav Stefanov <stanislav.stefanov@gmail.com>
2026-07-16 22:48:05 +0300

    feat(angular): add /financial route and Finance nav link

--- ui/angular-prototype/src/app/app.routes.ts
+++ ui/angular-prototype/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DirectoryListComponent } from './directory/directory-list.component';
 import { ReservationsComponent } from './reservations/reservations.component';
+import { FinancialComponent } from './financial/financial.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'directory', pathMatch: 'full' },
   { path: 'directory', component: DirectoryListComponent },
   { path: 'reservations', component: ReservationsComponent },
+  { path: 'financial', component: FinancialComponent },
 ];
